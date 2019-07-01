686fad44864803b5fd020210e87a984c54f38d5f
Administrador <Administrador@10.10.4.4>
2019-07-01 17:27:52 -0300

    Versão beta

---
+++ professor/src/modelo/Professor.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Professor {
+    private int codigo;
+    private String nome;
+    private String sobrenome;
+    private String endereco;
+
+    public Professor() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;  
+    } 
+
+    public Professor(String nome, String sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" + "nome=" + nome + ", sobrenome=" + sobrenome + ", endereco=" + endereco + '}';
+    }
+}
+
+
